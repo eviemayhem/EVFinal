95d398a58eb208c296ba6548557f6da13fba40f4
Evie Iles <iles.hollyl@gmail.com>
2022-04-16 10:10:21 -0400

    Worked on UI

--- evChecker.py
+++ evChecker.py
@@ -13,28 +13,40 @@ def main():
     root.title("EV Checker")
     root.geometry("500x500")
 
+#---Creates new Window for Admin Dash ---#
+    def openAdminDash():
+        adminDash = Toplevel(root)
+        adminDash.title("Admin Dashboard")
+        adminDash.geometry("500x500")
 
 #---Ask Admin to Login---#
     def buildLogin():
-        
-        #---Authenticate Admin---#        
-        def authenticate():
-            if admin=="admin" and passWord =="admin":
-                print("Authenticated")
-            else:
-                print("Not Authenticated")
-
+        #Variables 
         adminLbl = Label(root, text="Admin Username")
         admin = Text(root, height=1, width=20)
         passLbl = Label(root, text="Admin Password")
-        passWord = Text(root, height=1, width=20)
+        passWord = Text(root, height=1, width=20) 
+        
+        #---Authenticate Admin---#     
+        #Grab Admin textbox entry and username textbox entry
+       
+        def authenticate(): 
+            if admin.get(1.0, "end-1c")=="admin" and passWord.get(1.0, "end-1c") =="admin":
+                print("Authenticated")
+                #Opens Admin Dashboard
+                openAdminDash()
+            else:
+                print("Not Authenticated")
+        #Button
         loginBtn = Button(root, text="Login", command=authenticate)
         adminLbl.pack()
         admin.pack()
         passLbl.pack()
         passWord.pack()
         loginBtn.pack()
-        
+       
+
+    
 
 
 
