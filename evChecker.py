26ecba035734c81cff65ce9cfd585349175368bc
Evie Iles <iles.hollyl@gmail.com>
2022-04-18 11:49:06 -0400

    Added Listbox

--- evChecker.py
+++ evChecker.py
@@ -4,6 +4,7 @@ from typing import final
 # Software Analysis final
 
 #Import tkinter modules
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
@@ -13,11 +14,36 @@ def main():
     root.title("EV Checker")
     root.geometry("500x500")
 
+#---Constant Variables---#
+
+    maintList = ("Battery #5679", "Battery #3565")
+
+    maint_var = tk.StringVar(value=maintList)
+
+    
+       
 #---Creates new Window for Admin Dash ---#
     def openAdminDash():
         adminDash = Toplevel(root)
         adminDash.title("Admin Dashboard")
         adminDash.geometry("500x500")
+        
+        maintenanceLbl = Label(adminDash, text="Maintenance Logs")
+        maintenanceLbl.pack()
+        
+        #create Listbox for Maintenance Logs
+        maintLB= tk.Listbox(adminDash, listvariable=maint_var, height=5, selectmode='extended')
+        maintLB.pack(pady=5)
+
+        #---Event Handler for maintenance Listbox---#
+        def batteryselect(event):
+            print("Button clicked")
+
+        #Get selected index
+        selected_index = maintLB.curselection()
+        #Get selected maintenance item
+        selected_item = maintLB.get(selected_index)
+        
 
 #---Ask Admin to Login---#
     def buildLogin():
@@ -39,6 +65,7 @@ def main():
                 print("Not Authenticated")
         #Button
         loginBtn = Button(root, text="Login", command=authenticate)
+        #---Places Widgets---#
         adminLbl.pack()
         admin.pack()
         passLbl.pack()
